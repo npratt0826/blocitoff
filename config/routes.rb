dc00558ec0b36acff13402a5a9819b0dee293a66
Nick Pratt <nicholasapratt@gmail.com>
2017-10-05 14:56:53 -0400

    user show completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'welcome#index'
-
-  get 'welcome/index'
-
+  resources :users, only: [:show]
   get 'welcome/about'
+  get 'welcome/index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
